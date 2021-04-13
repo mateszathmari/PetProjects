a05d58aa72498c6d40bbdd5814378cc4317ed230
Mate Szathmari <mateszathmari@gmail.com>
2021-04-13 08:33:01 +0200

    Search on page

--- recipes/src/App.js
+++ recipes/src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { useState, useEffect } from "react";
 import Recipe from "./Recipe";
@@ -7,6 +6,7 @@ function App() {
   const API_ID = "";
   const API_KEY = "";
   const EXAMPLEQUERRY = `https://api.edamam.com/search?q=chicken&app_id=${API_ID}&app_key=${API_KEY}`;
+
   //"https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free"
 
   // const getInfo = () => {
@@ -18,24 +18,47 @@ function App() {
   //       console.error(err);
   //     });
   // };
+
   const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("chicken");
 
   useEffect(() => {
     getRecipes();
-  }, []);
+  }, [query]);
 
   const getRecipes = async () => {
     const response = await fetch(
-      `https://api.edamam.com/search?q=chicken&app_id=${API_ID}&app_key=${API_KEY}`
+      `https://api.edamam.com/search?q=${query}&app_id=${API_ID}&app_key=${API_KEY}&from=0&to=3`
     );
     const data = await response.json();
-    console.log(data);
     setRecipes(data.hits);
+  };
+
+  const UpdateSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
   };
 
   return (
     <div className="App">
-      <p>asd</p>
+      <form onSubmit={getSearch}>
+        <input
+          className="search-bar"
+          type="text"
+          onChange={UpdateSearch}
+          placeholder="search here"
+          value={search}
+        />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
       {recipes.map((recipe) => (
         <Recipe key={recipe.recipe.label} image={recipe.recipe.image} />
       ))}
