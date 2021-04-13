78dc8265dd8cbee6d7028b9def40e46d789d8250
Mate Szathmari <mateszathmari@gmail.com>
2021-04-13 19:11:23 +0200

    Load more recibes by click

--- recipes/src/App.js
+++ recipes/src/App.js
@@ -4,9 +4,9 @@ import Recipe from "./components/Recipe";
 import GetRecipes from "./hooks/GetRecipes";
 
 function App() {
-  const API_ID = "8cfa623e";
-  const API_KEY = "a3dc989b7a01df6e08dd2567b0af1abd";
-  const EXAMPLEQUERRY = `https://api.edamam.com/search?q=chicken&app_id=${API_ID}&app_key=${API_KEY}`;
+  // const API_ID = "8cfa623e";
+  // const API_KEY = "a3dc989b7a01df6e08dd2567b0af1abd";
+  // const EXAMPLEQUERRY = `https://api.edamam.com/search?q=chicken&app_id=${API_ID}&app_key=${API_KEY}`;
 
   //"https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free"
 
@@ -19,20 +19,20 @@ function App() {
   //       console.error(err);
   //     });
   // };
-  const [isLoading, fetchedData] = GetRecipes("chicken", []);
 
-  const [recipes, setRecipes] = useState([]);
-  const [drecipes, setdRecipes] = useState([]);
   const [search, setSearch] = useState("");
   const [query, setQuery] = useState("chicken");
+  const [from, setFrom] = useState(0);
+  const [to, setTo] = useState(10);
 
-  useEffect(() => {
-    //getRecipes();
-  }, [query]);
+  let [isLoading, fetchedData] = GetRecipes(query, from, to, [query, to]);
+  // useEffect(() => {
+  //   getRecipes();
+  // }, [query]);
 
-  useEffect(() => {
-    document.addEventListener("scroll", refreshEvent);
-  }, []);
+  // useEffect(() => {
+  //   document.addEventListener("scroll", refreshEvent);
+  // }, []);
 
   // const getRecipes = async (from = 0, to = 10) => {
   //   const response = await fetch(
@@ -52,36 +52,51 @@ function App() {
     setSearch("");
   };
 
-  const refreshEvent = async (e) => {
-    let app = document.getElementsByClassName("recipes");
-    console.log(app[0].offsetHeight + "max");
-    console.log(window.pageYOffset + " actual");
-    if (app[0].offsetHeight === window.pageYOffset + 500) {
-      alert("reached the bottom");
-    }
+  // const refreshEvent = async (e) => {
+  //   let app = document.getElementsByClassName("recipes");
+  //   console.log(app[0].offsetHeight + "max");
+  //   console.log(window.pageYOffset + " actual");
+  //   if (app[0].offsetHeight === window.pageYOffset + 500) {
+  //     alert("reached the bottom");
+  //   }
+  // };
+
+  const loadMore = () => {
+    setTo(to + 10);
   };
 
-  return (
-    <div className="App">
-      <form onSubmit={getSearch}>
-        <input
-          className="search-bar"
-          type="text"
-          onChange={UpdateSearch}
-          placeholder="search here"
-          value={search}
-        />
-        <button className="search-button" type="submit">
-          Search
-        </button>
-      </form>
-      <div className="recipes">
-        {recipes.map((recipe) => (
-          <Recipe key={recipe.recipe.label} image={recipe.recipe.image} />
-        ))}
+  let content = <div className="App">Loading Recipes...</div>;
+
+  if (!isLoading) {
+    content = (
+      <div className="App">
+        <form onSubmit={getSearch}>
+          <input
+            className="search-bar"
+            type="text"
+            onChange={UpdateSearch}
+            placeholder="search here"
+            value={search}
+          />
+          <button className="search-button" type="submit">
+            Search
+          </button>
+        </form>
+        <div className="recipes">
+          {fetchedData.map((
+            recipe //{recipes.map((recipe) => (
+          ) => (
+            <Recipe key={recipe.recipe.label} image={recipe.recipe.image} />
+          ))}
+        </div>
+        <div className="loadMore" onClick={loadMore}>
+          Click For More Content
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+
+  return content;
 }
 
 export default App;
