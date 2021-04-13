1b64bca6802515a99442734cdb688a9d57cd807a
Mate Szathmari <mateszathmari@gmail.com>
2021-04-13 11:43:44 +0200

    Rearrange project stucture

--- recipes/src/components/App.js
+++ recipes/src/components/App.js
@@ -20,6 +20,7 @@ function App() {
   // };
 
   const [recipes, setRecipes] = useState([]);
+  const [drecipes, setdRecipes] = useState([]);
   const [search, setSearch] = useState("");
   const [query, setQuery] = useState("chicken");
 
@@ -27,9 +28,13 @@ function App() {
     getRecipes();
   }, [query]);
 
-  const getRecipes = async () => {
+  useEffect(() => {
+    document.addEventListener("scroll", refreshEvent);
+  }, []);
+
+  const getRecipes = async (from = 0, to = 10) => {
     const response = await fetch(
-      `https://api.edamam.com/search?q=${query}&app_id=${API_ID}&app_key=${API_KEY}&from=0&to=3`
+      `https://api.edamam.com/search?q=${query}&app_id=${API_ID}&app_key=${API_KEY}&from=${from}&to=${to}`
     );
     const data = await response.json();
     setRecipes(data.hits);
@@ -43,6 +48,16 @@ function App() {
     e.preventDefault();
     setQuery(search);
     setSearch("");
+  };
+
+  const refreshEvent = async (e) => {
+    let app = document.getElementsByClassName("recipes");
+    console.log(app[0].offsetHeight + "max");
+    console.log(window.pageYOffset + " actual");
+    if (app[0].offsetHeight === window.pageYOffset + 500) {
+      setSearch(new Array(...recipes, ...recipes));
+      //alert("reached the bottom");
+    }
   };
 
   return (
@@ -59,9 +74,11 @@ function App() {
           Search
         </button>
       </form>
-      {recipes.map((recipe) => (
-        <Recipe key={recipe.recipe.label} image={recipe.recipe.image} />
-      ))}
+      <div className="recipes">
+        {recipes.map((recipe) => (
+          <Recipe key={recipe.recipe.label} image={recipe.recipe.image} />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ recipes/src/hooks/getRecipes.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function getRecipes() {
+  return <div></div>;
+}
