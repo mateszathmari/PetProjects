443a5d4d638f915fcac047877259f315e01069ba
Mate Szathmari <mateszathmari@gmail.com>
2021-04-15 17:46:37 +0200

    Add basic about page

---
+++ recipes/src/components/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "../pic/logo.png";
+
+export default function About() {
+  return (
+    <div className="About">
+      <img src={logo} alt="logo" width="200px" />
+    </div>
+  );
+}
