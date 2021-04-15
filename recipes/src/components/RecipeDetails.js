0b345c4ce5c296607b1173dcb8ba9ac520f85a39
Mate Szathmari <mateszathmari@gmail.com>
2021-04-15 10:48:59 +0200

    Basic styling for recipe details page

--- recipes/src/components/RecipeDetails.js
+++ recipes/src/components/RecipeDetails.js
@@ -1,5 +1,6 @@
 import { React } from "react";
 import { useStoreState } from "easy-peasy";
+import "../static/RecipeDetails.css";
 
 export default function RecipeDetails() {
   const recipe = useStoreState((state) => state.actualView.recipe);
@@ -8,9 +9,41 @@ export default function RecipeDetails() {
 
   if (recipe.label !== "") {
     content = (
-      <div>
-        <div>{recipe.label}</div>
-        <img src={recipe.image} alt="Food" />
+      <div className="Recipe">
+        <div className="Table">
+          <table className="Column">
+            <tr>
+              <div className="Food-title">{recipe.label}</div>
+            </tr>
+            <tr>
+              <div className="Total-time">{recipe.totalTime} mins</div>
+            </tr>
+            <tr>
+              <img className="recipe-image" src={recipe.image} alt="Food" />
+            </tr>
+            <tr>
+              <th>
+                <label>Ingredients</label>
+                <dl>
+                  {recipe.ingredientLines.map((ingredientLine) => (
+                    <dt>{ingredientLine}</dt>
+                  ))}
+                </dl>
+              </th>
+              <th>
+                <label>Health labels</label>
+                <dl>
+                  {recipe.healthLabels.map((label) => (
+                    <dt>{label}</dt>
+                  ))}
+                </dl>
+              </th>
+            </tr>
+            <tr>
+              <a href={recipe.url}>{recipe.url}</a>
+            </tr>
+          </table>
+        </div>
       </div>
     );
   }
