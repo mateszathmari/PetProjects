345ce2470f2da6b77877c630d29f97e3896c9a4d
Mate Szathmari <mateszathmari@gmail.com>
2021-04-15 07:17:51 +0200

    Link to recipe details

--- recipes/src/components/Recipe.js
+++ recipes/src/components/Recipe.js
@@ -1,20 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { useStoreState, useStoreActions } from "easy-peasy";
 
 const Recipe = ({ label, image }) => {
   const setRecipeImage = useStoreActions((state) => state.actualView.setImg);
   const setLabel = useStoreActions((state) => state.actualView.setLabel);
+
   const details = (e) => {
     setRecipeImage(image);
     setLabel(label);
-    window.location = "/recipe-details";
   };
 
   return (
-    <div className="recipe" onClick={details}>
-      <img className="recipe-image" src={image} alt="Food" />
-      <div className="title-text">{label}</div>
-    </div>
+    <Link to={`/recipe-details`}>
+      <div className="recipe" onClick={details}>
+        <img className="recipe-image" src={image} alt="Food" />
+        <div className="title-text">{label}</div>
+      </div>
+    </Link>
   );
 };
 
