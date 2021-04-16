52b65bfa716e04153ef59f42756150a7824800e1
Mate Szathmari <mateszathmari@gmail.com>
2021-04-16 11:43:38 +0200

    Add home component to app component

--- recipes/src/App.js
+++ recipes/src/App.js
@@ -9,17 +9,13 @@ import NextContent from "./components/NextContent";
 import RecipeDetails from "./components/RecipeDetails";
 import NavigationBar from "./components/NavigationBar";
 import About from "./components/About";
+import Home from "./components/Home";
 
 function App() {
   const from = useStoreState((state) => state.search.from);
   const to = useStoreState((state) => state.search.to);
   const query = useStoreState((state) => state.search.query);
   const setQuery = useStoreActions((actions) => actions.search.setQuery);
-
-  if (query === "") {
-    setQuery("chicken");
-  }
-
   let [fetchedData] = GetRecipes(query, from, to, [query, to]);
 
   return (
@@ -27,7 +23,9 @@ function App() {
       <Router>
         <NavigationBar />
         <Switch>
-          <Route exact path="/"></Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
           <Route path="/about" component={About} />
 
           <Route path="/recipe-details">
