47e734c5b261af30bc6cf8ca647835f8b2534df3
Mate Szathmari <mateszathmari@gmail.com>
2021-04-16 14:12:07 +0200

    Make basic login component

--- recipes/src/components/Login.js
+++ recipes/src/components/Login.js
@@ -1,5 +1,19 @@
 import React from "react";
 
 export default function Login() {
-  return <div></div>;
+  const login = (e) => {
+    e.preventDefault();
+    console.log(e.target.username.value);
+    console.log(e.target.password.value);
+  };
+
+  return (
+    <div className="Login">
+      <form onSubmit={login}>
+        <input type="text" name="username" placeholder="Username" />
+        <input type="password" name="password" placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
