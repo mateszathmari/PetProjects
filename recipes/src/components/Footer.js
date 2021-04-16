245727ee1fcb6ebc27ebf68214f30bd894303a52
Mate Szathmari <mateszathmari@gmail.com>
2021-04-16 13:38:41 +0200

    Delete unused code

--- recipes/src/components/Footer.js
+++ recipes/src/components/Footer.js
@@ -10,21 +10,11 @@ import vs from "../pic/vs.png";
 export default function Footer() {
   return (
     <div className="Footer">
-      <img className="logo" src={logo} alt="logo" width="80px" />
       <img className="logo" src={easyPeasy} alt="logo" height="40px" />
       <img className="logo" src={htmlCss} alt="logo" height="40px" />
       <img className="logo" src={github} alt="logo" height="40px" />
       <img className="logo" src={react} alt="logo" height="40px" />
       <img className="logo" src={vs} alt="logo" height="40px" />
-      {/* <Link className="Navigation-text" to={"/"}>
-        Home{" "}
-      </Link>
-      <Link className="Navigation-text" to={"/recipes"}>
-        Recipes
-      </Link>
-      <Link className="Navigation-text" to={"/about"}>
-        About
-      </Link> */}
     </div>
   );
 }
