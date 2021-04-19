75fd8b7075ef153cc2e6f2147be4a136568597de
Mate Szathmari <mateszathmari@gmail.com>
2021-04-19 07:22:37 +0200

    Update about page with picture and link to github

--- recipes/src/components/About.js
+++ recipes/src/components/About.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import logo from "../pic/logo.png";
+import developer from "../pic/developer.png";
 
 export default function About() {
   return (
     <div className="About">
       <img src={logo} alt="logo" width="200px" />
+      <a href={"https://github.com/mateszathmari"}>
+        <img
+          className="dev-pic"
+          src={developer}
+          alt="developer"
+          width="100px"
+        />
+      </a>
     </div>
   );
 }
