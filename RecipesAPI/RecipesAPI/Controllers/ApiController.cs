fef0028a52111a86edefc310e5486362ee183051
Mate Szathmari <mateszathmari@gmail.com>
2021-04-22 07:47:47 +0200

    Add new empty backend API

---
+++ RecipesAPI/RecipesAPI/Controllers/ApiController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RecipesAPI.Models;
+
+namespace RecipesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ApiController : ControllerBase
+    {
+        private readonly ILogger<ApiController> _logger;
+
+        public ApiController(ILogger<ApiController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpGet()]
+        public string Get()
+        {
+            return "Data.";
+        }
+    }
+}
