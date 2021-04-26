8378fa6575182d577c21d7206e6c479b2373eb74
Mate Szathmari <mateszathmari@gmail.com>
2021-04-26 12:04:27 +0200

    Add recipe modell

--- RecipesAPI/EFDataAccess/Models/Recipe.cs
+++ RecipesAPI/EFDataAccess/Models/Recipe.cs
@@ -1,9 +1,13 @@
 ﻿
 
+using System.ComponentModel.DataAnnotations;
+
 namespace RecipesAPI.Models
 {
     public class Recipe
     {
-        public string Name = "";
+        [Key] public int id { get; set; }
+        [Required]
+        public string Name { get; set; }
     }
 }
