0094380de3c92959b505a0118d9e75210e1d3d10
Mate Szathmari <mateszathmari@gmail.com>
2021-04-26 12:04:45 +0200

    Add user model

--- RecipesAPI/EFDataAccess/Models/User.cs
+++ RecipesAPI/EFDataAccess/Models/User.cs
@@ -1,37 +1,38 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Security.Principal;
 
 namespace RecipesAPI.Models
 {
     public class User
     {
-        private string _userName { get; }
+        
         [Key]
         public int Id { get; set; }
-        private string _email { get; }
-        private string _address { get; }
-        private string _token;
+        [Required] [MaxLength(100)]
+        public string UserName { get; }
+        [Required]
+        [MaxLength(200)]
+        public string Email { get; set; }
+        [Required]
+        [MaxLength(300)]
+        public Address Address { get; set; }
+        public Token TokenString { get; set; }
+        public List<Recipe> Recipes { get; set; } = new List<Recipe>();
 
         public User() { }
 
-        public User(string userName, string email, string address)
+        public User(string userName, string email, Address address)
         {
-            _address = address;
-            _userName = userName;
-            _email = email;
+            Address = address;
+            UserName = userName;
+            Email = email;
         }
 
-        public string Token
-        {
-            set
-            {
-                //token = generateTokenForUser();
-            }
-        }
 
-        public bool IsCorrectToken(string givenToken)
+        public bool IsCorrectToken(Token givenToken)
         {
-            return _token == givenToken;
+            return TokenString == givenToken;
         }
     }
 }
