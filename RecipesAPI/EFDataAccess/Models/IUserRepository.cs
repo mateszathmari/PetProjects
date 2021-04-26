cfa8f32ca8815fdc2757c8ceba4d36b92346aaa7
Mate Szathmari <mateszathmari@gmail.com>
2021-04-26 12:06:39 +0200

    Add SQL User repository and IUserRepository interface

---
+++ RecipesAPI/EFDataAccess/Models/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace RecipesAPI.Models
+{
+    public interface IUserRepository
+    {
+        User GetUser(int id);
+        IEnumerable<User> GetUsers();
+        User AddUser(User person);
+        User updateUser(User personChanges);
+        User DeleteUser(int id);
+    }
+}
---
+++ RecipesAPI/EFDataAccess/Models/SQLUserRepository.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using EFDataAccess.DataAccess;
+
+namespace RecipesAPI.Models
+{
+    public class SQLUserRepository: IUserRepository
+    {
+        private readonly UserContext _context;
+
+        public SQLUserRepository(UserContext context)
+        {
+            _context = context;
+        }
+
+        public User GetUser(int id)
+        {
+            return _context.Users.Find(id);
+        }
+
+        public IEnumerable<User> GetUsers()
+        {
+            return _context.Users;
+        }
+
+        public User AddUser(User user)
+        {
+            _context.Users.Add(user);
+            _context.SaveChanges();
+            return user;
+        }
+
+        public User updateUser(User userChanges)
+        {
+            var user = _context.Users.Attach(userChanges);
+            user.State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _context.SaveChanges();
+            return userChanges;
+        }
+
+        public User DeleteUser(int id)
+        {
+            User user = _context.Users.Find(id);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+                _context.SaveChanges();
+            }
+
+            return user;
+        }
+    }
+}
