dd79841f7108713806b5e376b145e13fa6a3bac9
Mate Szathmari <mateszathmari@gmail.com>
2021-04-26 12:05:09 +0200

    Add Token model

---
+++ RecipesAPI/EFDataAccess/Models/Token.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecipesAPI.Models
+{
+    public class Token
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required][MaxLength(50)]
+        public string TokenString { get; set; }
+    }
+}
