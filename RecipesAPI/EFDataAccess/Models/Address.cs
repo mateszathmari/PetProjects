b67183b74c4367d031d5d5b2233a17cbdcda1337
Mate Szathmari <mateszathmari@gmail.com>
2021-04-26 12:04:00 +0200

    Add address modell

---
+++ RecipesAPI/EFDataAccess/Models/Address.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecipesAPI.Models
+{
+    public class Address
+    {
+        [Key] public int Id { get; set; }
+        [Required] [MaxLength(100)] public string City { get; set; }
+        [Required] [MaxLength(200)] public string Street { get; set; }
+        [Required] [MaxLength(10)] public int HouseNumber { get; set; }
+        [Required] [MaxLength(10)] public string PostCode { get; set; }
+
+        public Address(string city, string street, int houseNumber, string postCode)
+        {
+            City = city;
+            Street = street;
+            HouseNumber = houseNumber;
+            PostCode = postCode;
+        }
+    }
+}
