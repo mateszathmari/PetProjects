45e210523f80a4a890c9dd55d0b1c0d96870c103
Mate Szathmari <mateszathmari@gmail.com>
2021-04-28 14:34:37 +0200

    Add Ingredients label

---
+++ RecipesAPI/EFDataAccess/Models/Ingredient.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecipesAPI.Models
+{
+    public class Ingredient
+    {
+        [Key]
+        [MaxLength(100)]
+        public string Name { get; set; }
+    }
+}
