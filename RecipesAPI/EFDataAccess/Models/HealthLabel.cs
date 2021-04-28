9adac0c5c0cb9bee2ab1339c2e9c3321a02e1462
Mate Szathmari <mateszathmari@gmail.com>
2021-04-28 14:34:18 +0200

    Add Health labels model

---
+++ RecipesAPI/EFDataAccess/Models/HealthLabel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecipesAPI.Models
+{
+    public class HealthLabel
+    {
+
+        [Key]
+        [MaxLength(100)]
+        public string Name { get; set; }
+    }
+}
