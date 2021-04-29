af53c0d35c2e6b99d52cbb9ec483a9e53dc89e5c
Mate Szathmari <mateszathmari@gmail.com>
2021-04-29 16:22:30 +0200

    Add User credential class

---
+++ RecipesAPI/EFDataAccess/Models/UserCred.cs
@@ -0,0 +1,14 @@
+﻿namespace RecipesAPI.Models
+{
+    public class UserCred
+    {
+        public string Username { get; set; }
+        public string  Password { get; set; }
+
+        public UserCred(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+    }
+}
