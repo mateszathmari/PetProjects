859b682abbc4f8a545547a3c2559d2fd9ec9033a
Mate Szathmari <mateszathmari@gmail.com>
2021-04-30 16:07:54 +0200

    Add logout and registration endpoint test

--- RecipesAPI/Recipes-backend.tests/ApiControllerTests.cs
+++ RecipesAPI/Recipes-backend.tests/ApiControllerTests.cs
@@ -4,6 +4,7 @@ using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using EFDataAccess.DataAccess;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Newtonsoft.Json;
 using NUnit.Framework;
@@ -24,14 +25,60 @@ namespace Recipes_backend.tests
         }
 
         [Test]
-        public async Task Login_ValidCredential_ShouldReturnOk()
+        public async Task Test1_Registration_ValidCredential_ShouldReturnOk()
+        {
+            // Arrange
+            string url = "api/registration";
+            Address address = new Address("city", "street", 55, "11");
+            RegistrationCred registrationCred = new RegistrationCred("test@gmail.com", "test","test@test.com", "city",  "street", 11,  "postCode");
+            string output = JsonConvert.SerializeObject(registrationCred);
+            var req = new HttpRequestMessage(HttpMethod.Post, url)
+            {
+                Content = new StringContent(output,
+                    Encoding.UTF8, "application/json")
+            };
+
+            // Act
+            var response = await _client.SendAsync(req);
+
+            // Assert
+            Assert.IsTrue(response.StatusCode == HttpStatusCode.OK);
+        }
+
+        [Test]
+        public async Task Test2_Login_ValidCredential_ShouldReturnOk()
         {
             // Arrange
             string url = "api/login";
             UserCred userCred = new UserCred("mateszathmari@gmail.com", "1234");
             string output = JsonConvert.SerializeObject(userCred);
-            var req = new HttpRequestMessage(HttpMethod.Post, url) { Content = new StringContent(output,
-                Encoding.UTF8, "application/json") };
+            var req = new HttpRequestMessage(HttpMethod.Post, url)
+            {
+                Content = new StringContent(output,
+                    Encoding.UTF8, "application/json")
+            };
+
+            // Act
+            var response = await _client.SendAsync(req);
+
+            // Assert
+            Assert.IsTrue(response.StatusCode == HttpStatusCode.OK);
+        }
+
+        [Test]
+        public async Task Test3_Logout_ValidCredential_ShouldReturnOk()
+        {
+            // Arrange
+            string url = "api/logout";
+            AuthenticationCred userCred =
+                new AuthenticationCred("mateszathmari@gmail.com",
+                    "AZw+xb0L2Ugyq7KGDyS9RqoHgr1GF/eR"); // we should get the token
+            string output = JsonConvert.SerializeObject(userCred);
+            var req = new HttpRequestMessage(HttpMethod.Post, url)
+            {
+                Content = new StringContent(output,
+                    Encoding.UTF8, "application/json")
+            };
 
             // Act
             var response = await _client.SendAsync(req);
