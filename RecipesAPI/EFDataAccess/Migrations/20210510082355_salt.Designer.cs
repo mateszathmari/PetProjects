596666bf2ddcbf04aa3fd6523feaa7197dd238bc
Mate Szathmari <mateszathmari@gmail.com>
2021-05-10 10:25:32 +0200

    Add unique salt for every user

--- RecipesAPI/EFDataAccess/Migrations/20210510082355_salt.Designer.cs
+++ RecipesAPI/EFDataAccess/Migrations/20210510082355_salt.Designer.cs
@@ -4,14 +4,16 @@ using EFDataAccess.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EFDataAccess.Migrations
 {
     [DbContext(typeof(UserContext))]
-    partial class UserContextModelSnapshot : ModelSnapshot
+    [Migration("20210510082355_salt")]
+    partial class salt
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
