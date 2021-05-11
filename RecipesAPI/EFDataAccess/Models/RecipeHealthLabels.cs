84a30b81449343ef9418cb8d244f2a7233b55f07
Mate Szathmari <mateszathmari@gmail.com>
2021-05-11 14:55:22 +0200

    Add models to make database relationships

---
+++ RecipesAPI/EFDataAccess/Models/RecipeHealthLabels.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecipesAPI.Models
+{
+    public class RecipeHealthLabels
+    {
+        [Key] public int Id { get; set; }
+        public int HealthLabelId { get; set; }
+        public HealthLabel HealthLabel { get; set; }
+        public int RecipeId { get; set; }
+        public Recipe Recipe { get; set; }
+
+        public RecipeHealthLabels()
+        {
+        }
+
+        public RecipeHealthLabels(HealthLabel healthLabel, Recipe recipe)
+        {
+            HealthLabelId = healthLabel.Id;
+            HealthLabel = healthLabel;
+            RecipeId = recipe.id;
+            Recipe = recipe;
+        }
+    }
+}
---
+++ RecipesAPI/EFDataAccess/Models/RecipeIngredients.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecipesAPI.Models
+{
+    public class RecipeIngredients
+    {
+        [Key] public int Id { get; set; }
+        public int IngredientId { get; set; }
+        public Ingredient Ingredient { get; set; }
+        public int RecipeId { get; set; }
+        public Recipe Recipe { get; set; }
+
+        public RecipeIngredients()
+        {
+        }
+
+        public RecipeIngredients(Ingredient ingredient, Recipe recipe)
+        {
+            IngredientId = ingredient.Id;
+            Ingredient = ingredient;
+            RecipeId = recipe.id;
+            Recipe = recipe;
+        }
+    }
+}
