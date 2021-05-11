ba6f7a6dfc63a5f1a49ab2d65d0739b1624899f7
Mate Szathmari <mateszathmari@gmail.com>
2021-05-11 14:59:13 +0200

    Add constructor for health labels

--- RecipesAPI/EFDataAccess/Models/HealthLabel.cs
+++ RecipesAPI/EFDataAccess/Models/HealthLabel.cs
@@ -1,12 +1,23 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace RecipesAPI.Models
 {
     public class HealthLabel
     {
+        [Key] public int Id { get; set; }
+        [MaxLength(100)] public string Name { get; set; }
 
-        [Key]
-        [MaxLength(100)]
-        public string Name { get; set; }
+        // public HashSet<Recipe> Recipes { get; set; } = new HashSet<Recipe>();
+        public ICollection<RecipeHealthLabels> RecipeHealthLabels { get; set; } = new HashSet<RecipeHealthLabels>();
+
+        public HealthLabel()
+        {
+        }
+
+        public HealthLabel(string name)
+        {
+            Name = name;
+        }
     }
 }
