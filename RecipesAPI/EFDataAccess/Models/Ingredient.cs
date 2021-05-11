0c9f47527dbd5e4ffbd2800cf54d2ff23133c444
Mate Szathmari <mateszathmari@gmail.com>
2021-05-11 14:59:32 +0200

    Add constructor for ingredients

--- RecipesAPI/EFDataAccess/Models/Ingredient.cs
+++ RecipesAPI/EFDataAccess/Models/Ingredient.cs
@@ -1,11 +1,24 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace RecipesAPI.Models
 {
     public class Ingredient
     {
-        [Key]
-        [MaxLength(100)]
-        public string Name { get; set; }
+        [Key] public int Id { get; set; }
+
+        [MaxLength(100)] public string Name { get; set; }
+
+        //public List<Recipe> Recipes { get; set; } = new List<Recipe>();
+        public ICollection<RecipeIngredients> RecipeIngredients { get; set; } = new HashSet<RecipeIngredients>();
+
+        public Ingredient()
+        {
+        }
+
+        public Ingredient(string name)
+        {
+            Name = name;
+        }
     }
 }
