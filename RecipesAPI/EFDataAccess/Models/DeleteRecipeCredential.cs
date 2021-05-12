216434a6e683c8577c5a4b1100a761fe28ae6fde
Mate Szathmari <mateszathmari@gmail.com>
2021-05-12 12:58:03 +0200

    Add "DeleteRecipeCredential" class

---
+++ RecipesAPI/EFDataAccess/Models/DeleteRecipeCredential.cs
@@ -0,0 +1,14 @@
+﻿namespace RecipesAPI.Models
+{
+    public class DeleteRecipeCredential
+    {
+        public AuthenticationCredential AuthenticationCredential { get; set; }
+        public int RecipeId { get; set; }
+
+        public DeleteRecipeCredential(AuthenticationCredential authenticationCredential, int recipeId)
+        {
+            AuthenticationCredential = authenticationCredential;
+            RecipeId = recipeId;
+        }
+    }
+}
