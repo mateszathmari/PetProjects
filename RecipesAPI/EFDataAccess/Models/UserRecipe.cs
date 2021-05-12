9402b71dff97a7e4cbce30c73d7a40c009363b8b
Mate Szathmari <mateszathmari@gmail.com>
2021-05-12 12:55:42 +0200

    rename UserRecipes to UserRecipe

---
+++ RecipesAPI/EFDataAccess/Models/UserRecipe.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecipesAPI.Models
+{
+    public class UserRecipe
+    {
+        [Key] public int Id { get; set; }
+        [MaxLength(100)] public string UserName { get; set; }
+        public User User { get; set; }
+        public int RecipeId { get; set; }
+        public Recipe Recipe { get; set; }
+
+        public UserRecipe()
+        {
+            
+        }
+
+        public UserRecipe(User user, Recipe recipe)
+        {
+
+            UserName = user.UserName;
+            User = user;
+            RecipeId = recipe.id;
+            Recipe = recipe;
+        }
+    }
+}
