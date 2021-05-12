bad0adfea6f8c08fea5733c9f4950c43aca175a7
Mate Szathmari <mateszathmari@gmail.com>
2021-05-12 12:56:04 +0200

    Add migration many to many

---
+++ RecipesAPI/EFDataAccess/Migrations/20210512072551_manyToMany.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFDataAccess.Migrations
+{
+    public partial class manyToMany : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_HealthLabels_Recipes_Recipeid",
+                table: "HealthLabels");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Ingredients_Recipes_Recipeid",
+                table: "Ingredients");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Ingredients_Recipeid",
+                table: "Ingredients");
+
+            migrationBuilder.DropIndex(
+                name: "IX_HealthLabels_Recipeid",
+                table: "HealthLabels");
+
+            migrationBuilder.DropColumn(
+                name: "Recipeid",
+                table: "Ingredients");
+
+            migrationBuilder.DropColumn(
+                name: "Recipeid",
+                table: "HealthLabels");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Recipeid",
+                table: "Ingredients",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Recipeid",
+                table: "HealthLabels",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ingredients_Recipeid",
+                table: "Ingredients",
+                column: "Recipeid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HealthLabels_Recipeid",
+                table: "HealthLabels",
+                column: "Recipeid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_HealthLabels_Recipes_Recipeid",
+                table: "HealthLabels",
+                column: "Recipeid",
+                principalTable: "Recipes",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Ingredients_Recipes_Recipeid",
+                table: "Ingredients",
+                column: "Recipeid",
+                principalTable: "Recipes",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
